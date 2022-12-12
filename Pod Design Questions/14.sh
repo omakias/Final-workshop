f10c56ea8428626c9e27688845859ada343d554a
omakias <117858969+omakias@users.noreply.github.com>
2022-12-12 15:48:29 +0200

    Create 14.sh

---
+++ Pod Design Questions/14.sh
@@ -0,0 +1,4 @@
+kubectl get nodes  --show-labels
+
+NAME       STATUS   ROLES           AGE   VERSION   LABELS
+minikube   Ready    control-plane   29d   v1.25.3   beta.kubernetes.io/arch=amd64,beta.kubernetes.io/os=linux,env=prod,kubernetes.io/arch=amd64,kubernetes.io/hostname=minikube,kubernetes.io/os=linux,minikube.k8s.io/commit=986b1ebd987211ed16f8cc10aed7d2c42fc8392f,minikube.k8s.io/name=minikube,minikube.k8s.io/primary=true,minikube.k8s.io/updated_at=2022_11_13T14_01_17_0700,minikube.k8s.io/version=v1.28.0,node-role.kubernetes.io/control-plane=,node.kubernetes.io/exclude-from-external-load-balancers=
