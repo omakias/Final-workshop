70be89fd75b8d0b728918c948ea01cc2662de24f
omakias <117858969+omakias@users.noreply.github.com>
2022-12-12 16:08:33 +0200

    Update 15.sh

--- Pod Design Questions/15.sh
+++ Pod Design Questions/15.sh
@@ -1,2 +1,5 @@
-kubectl get nodes --show-labels
-kubectl label nodes worker nodeName=nginxnode
+kubectl label nodes minikube nodeName=nginxnode
+Verify - 
+
+NAME       STATUS   ROLES           AGE   VERSION   LABELS
+minikube   Ready    control-plane   29d   v1.25.3   beta.kubernetes.io/arch=amd64,beta....xclude-from-external-load-balancers=,nodeName=nginxnode
