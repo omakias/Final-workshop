e1a1c6e028b765073039d6f3a2fa044ec7060680
omakias <117858969+omakias@users.noreply.github.com>
2022-12-12 13:18:21 +0200

    Create 4.sh

---
+++ Pod Design Questions/4.sh
@@ -0,0 +1,5 @@
+kubectl get pods -l environment=dev
+NAME   READY   STATUS    RESTARTS   AGE
+pod3   1/1     Running   0          38s
+pod4   1/1     Running   0          38s
+pod5   1/1     Running   0          38s
