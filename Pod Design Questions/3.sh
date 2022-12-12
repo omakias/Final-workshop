1f139ebe2d06901fb07d0b196dccdd69bba2f3d0
omakias <117858969+omakias@users.noreply.github.com>
2022-12-12 11:53:27 +0200

    Create 3.sh

---
+++ Pod Design Questions/3.sh
@@ -0,0 +1,8 @@
+Verify pods labels 
+kubectl get po --show-labels
+##### result ####
+pod1   1/1     Running   0          5m30s   environment=prod
+pod2   1/1     Running   0          5m30s   environment=prod
+pod3   1/1     Running   0          5m30s   environment=dev
+pod4   1/1     Running   0          5m30s   environment=dev
+pod5   1/1     Running   0          5m30s   environment=dev
