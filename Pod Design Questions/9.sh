95530236efe78f1e9cc1d2ea696acca33efe884d
omakias <117858969+omakias@users.noreply.github.com>
2022-12-12 13:44:02 +0200

    Create 9.sh

---
+++ Pod Design Questions/9.sh
@@ -0,0 +1,7 @@
+kubectl get pod -l 'environment in (prod, dev)'
+NAME   READY   STATUS    RESTARTS   AGE
+pod1   1/1     Running   0          109m
+pod2   1/1     Running   0          109m
+pod3   1/1     Running   0          109m
+pod4   1/1     Running   0          109m
+pod5   1/1     Running   0          109m
